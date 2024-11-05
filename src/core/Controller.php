3320e88ed364521d74008009b9864110de4258d8
Giovanne Monteiro <101524886+Gmmbr10@users.noreply.github.com>
2024-11-05 18:55:23 -0300

    adicionando a funcao de chamar model

--- src/core/Controller.php
+++ src/core/Controller.php
@@ -1,18 +1,22 @@
 <?php
 
-class Controller {
+class Controller
+{
 
 	public function view($filename, $data = [])
 	{
 
 		$filename = str_replace(".", "/", $filename);
 
-		
-
 		require_once(__DIR__ . "/../view/default_templates/start_base.php");
 		require_once(__DIR__ . "/../view/" . $filename . ".php");
 		require_once(__DIR__ . "/../view/default_templates/end_base.html");
-
 	}
 
+	public function model($modelname)
+	{
+
+		require_once(__DIR__ . "/../model/" . $modelname . ".php");
+		return new $modelname;
+	}
 }
